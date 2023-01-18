e0eac9a9a4b99a1d0627e2f7c61bae242cc93c57
codewithmurshid <82975683+murshidameenpv@users.noreply.github.com>
2023-01-18 09:37:18 +0530

    fgfgfgf

--- app.js
+++ app.js
@@ -1,3 +1,4 @@
+
 (function () {
     [...document.querySelectorAll(".control")].forEach(button => {
         button.addEventListener("click", function() {
@@ -12,4 +13,93 @@
     })
 })();
 
-//submit form
+
+                            // --validation--
+
+
+function validateName() {
+   var text=document.getElementById("cname").value;
+   var regex1 = /^[A-Za-z]+$/;  
+
+   if(text == ""){
+    document.getElementById("name-error").innerHTML="**Name Can not be Empty";
+    document.getElementById("name-error").style.visibility="visible";
+    document.getElementById("name-error").style.color="red";
+   }
+   if(regex1.test(text)){
+    document.getElementById("name-error").innerHTML="valid";
+    document.getElementById("name-error").style.visibility="visible";
+    document.getElementById("name-error").style.color="green";
+   }
+};
+
+function validateEmail() {
+  var emailtext = document.getElementById("cemail").value;
+  var regex2 =/^([a-zA-Z0-9\.-]+)@([a-zA-Z0-9-]+).([a-z]{2,20})(.[a-z]{2,8})?$/ ;
+
+
+  if(regex2.test(emailtext)) {
+    document.getElementById("mail-error").innerHTML="valid";
+    document.getElementById("mail-error").style.visibility="visible";
+    document.getElementById("mail-error").style.color="green";
+ }
+ else{
+    document.getElementById("mail-error").innerHTML="**invalid";
+    document.getElementById("mail-error").style.visibility="visible";
+    document.getElementById("mail-error").style.color="red";
+ }
+};
+
+function validateSubject() {
+    var subtext=document.getElementById("csubject").value;
+    var regex3 = /^[A-Za-z]+$/;  
+ 
+    if(subtext == ""){
+     document.getElementById("subject-error").innerHTML="**Subject Can not be Empty";
+     document.getElementById("subject-error").style.visibility="visible";
+     document.getElementById("subject-error").style.color="red";
+    }
+    if(regex3.test(subtext)){
+     document.getElementById("subject-error").innerHTML="valid";
+     document.getElementById("subject-error").style.visibility="visible";
+     document.getElementById("subject-error").style.color="green";
+    }
+ };
+
+
+function validateMessage() {
+    var val = document.getElementById("cmessage").value;
+    console.log(val);
+    var v = null;
+    v = val.trim();
+    document.getElementById("cmessage").value = v;
+    var n = v.length;
+    console.log(n);
+    if (v == "" || val == null) {
+        document.getElementById("message-error").style.color = "red";
+        document.getElementById("message-error").innerHTML = "This field must be filled out";
+        return false;
+   }
+    if (n < 20) {
+        document.getElementById("message-error").style.color = "red";
+        document.getElementById("message-error").innerHTML = "Message should be atleast 10 letters long";
+        return false;
+
+
+    }
+    document.getElementById("message-error").style.color = "green";
+    document.getElementById("message-error").innerHTML = "Valid";
+    return true;
+
+}
+
+
+function validateForm() {
+    if (!validateEmail() || !validateName() || !validateMessage()) {
+        // submitError.style.display='block';
+        // submitError.innerHTML='Please fix error to submit';
+        // setTimeout(function(){submitError.style.display='none';},3000);
+        alert("Please fix the error to submit")
+        return  false;    
+    }
+}
